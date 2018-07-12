35bef7826102764b8a47bd7c6066b2db62758c49
greedbob <greedbob@foxmail.com>
2018-07-12 12:40:58 +0800

    练习1.3

---
+++ practice1.3.py
@@ -0,0 +1,11 @@
+import random
+
+guanci = ['a','the']
+zhuti = ['cat','dog','man','women']
+dongci = ['sang','ran','jumped']
+zhuangyu = ['loudly','quietly','well']
+
+if random.randint(0,2) < 1:
+    print(random.choice(guanci),random.choice(zhuti),random.choice(dongci),random.choice(zhuangyu))
+else:
+    print(random.choice(guanci),random.choice(zhuti),random.choice(dongci))
