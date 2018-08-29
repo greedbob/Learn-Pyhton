8274c43c90060c337e5f2f639fc6592693e7a338
greedbob <greedbob@foxmail.com>
2018-08-29 18:20:08 +0800

    fix bugs / 理解参考代码思路
    Signed-off-by: greedbob <greedbob@foxmail.com>

--- practice5.1.py
+++ practice5.1.py
@@ -1,69 +1,100 @@
-# Question:第五章 模块 5.4 练习 展示目录列表
-# Solution:
-# Answer:
+# Question:第五章 模块 5.4 练习 展示目录列表 相当于 dir/ls
+# Solution:创建组合数据，存储filename、size、modified，利用dict.item()函数将dict转化为set并sorted利用key函数排序
+# Answer:参考代码排序方法：
+# 获取所有文件名和目录名filenames、dirs
+# for name in filenames: 获取size modified
+# 构造元组，第一项为排序参照项，第二项为字符串filename、size、modified
+import locale
 import optparse
 import os
-import locale
+import datetime
+locale.setlocale(locale.LC_ALL, '')
 
 
 def main():
-    options, args= get_info()
+    options, args = get_info()
     get_files(options, args)
 
 
 def get_info():
-    parser = optparse.OptionParser()
+    usage = """ls.py [options] [path1 [path2 [... pathN]]]
+
+    The paths are optional; if not given . is used."""
+    parser = optparse.OptionParser(usage=usage)
     parser.add_option('-H', '--hidden', dest='hidden', action='store_true',
-                      help=('show hidden files [default:off]'))
+                      help='show hidden files [default:off]')
     parser.add_option('-m', '--modified', dest='modified', action='store_true',
-                      help=('show last modified date/time [default:off]'))
-    parser.add_option('-o', '--oder', dest='oder', action='store',
-                      help=('order by (''name'', ''n'', ''modified'', ''m'', ''size'', ''s'') default:name]'))
+                      help='show last modified date/time [default:off]')
+    parser.add_option('-o', '--oder', dest='order', action='store',  type='string',
+                      help='order by (''name'', ''n'', ''modified'', ''m'', ''size'', ''s'') [default:name]')
     parser.add_option('-r', '--recursive', dest='recursive', action='store_true',
-                      help=('recurse into subdirectories [default:off]'))
+                      help='recurse into subdirectories [default:off]')
     parser.add_option('-s', '--sizes', dest='sizes',  action='store_true',
-                      help=('show sizes [default:off]'))
-    parser.set_defaults(hidden=False, modified=False, order='name', recursive=False, sizes=False)
+                      help='show sizes [default:off]')
+    parser.set_defaults(order='name')
     options, args = parser.parse_args()
+    if not args:
+        args = ["."]
     return options, args
 
 
 def get_files(options, args):
     num_dirs = 0
-    num_files = 0
+    num_total = 0
     if not options.recursive:
         data = {}
+        ordered_data = {}
         for path in args:
             num_dirs += 1
             data[path] = {}
             for filename in os.listdir(path):
                 if not filename.startswith('.') or not options.hidden:
-                    num_files += 1
+                    num_total += 1
                     fullname = os.path.join(path, filename)
-                    if os.path.isfile(fullname):
-                        data[path][fullname] = os.path.getmtime(fullname), os.path.getsize(fullname)
-                        print_line(fullname, data[path][fullname], options)
+                    time = datetime.datetime.fromtimestamp(os.path.getmtime(fullname))
+                    size = os.path.getsize(fullname)
+                    data[path][fullname] = time.strftime('%Y-%m-%d %H:%M:%S'), size
+            ordered_data[path] = order_data(options.order, data[path])
+            print_path(ordered_data[path], options)
     if options.recursive:
         data = {}
+        ordered_data = {}
         for path in args:
             num_dirs += 1
             data[path] = {}
             for root, dirs, files in os.walk(path):
                 for filename in files:
-                    fullname = os.path.join(root, filename)
-                    if not fullname.startswith('.') or not options.hidden:
-                        num_files += 1
-                        data[path][fullname] = os.path.getmtime(fullname), os.path.getsize(fullname)
-                        print_line(fullname, data[path][fullname], options)
-    print('{0} files, {1} directory'.format(str(num_files), str(num_dirs)))
+                    if not filename.startswith('.') or not options.hidden:
+                        fullname = os.path.join(root, filename)
+                        num_total += 1
+                        time = datetime.datetime.fromtimestamp(os.path.getmtime(fullname))
+                        size = os.path.getsize(fullname)
+                        data[path][fullname] = time.strftime('%Y-%m-%d %H:%M:%S'), size
+            ordered_data[path] = order_data(options.order, data[path])
+            print_path(ordered_data[path], options)
+    print('{0} file{1}, {2} director{3}'.format(str(num_total-num_dirs) if num_total-num_dirs else 'no',
+                                                's' if num_total-num_dirs != 1 else '',
+                                                str(num_dirs) if num_dirs else 'no',
+                                                'ies' if num_dirs != 1 else 'y'))
 
 
-def print_line(fullname, msg, options):
-    line = ''
-    line += str(msg[0]) if options.modified else ''
-    line += str(msg[1]) if options.sizes else ''
-    line += fullname
-    print(line)
+def print_path(ordered_data, options):
+    for item in ordered_data:
+        line = ''
+        line += ('{0:>19} '.format(str(item[1][0]))) if options.modified else ''
+        line += ('{0:>15} '.format(str(item[1][1]))) if options.sizes else ''
+        line += str(item[0])
+        print(line)
+
+
+def order_data(order, data):
+    if order == 'size' or order == 's':
+        ordered_data = sorted(data.items(), key=lambda x: x[1][1])
+    elif order == 'modified' or order == 'm':
+        ordered_data = sorted(data.items(), key=lambda x: x[1][0])
+    else:
+        ordered_data = sorted(data.items(), key=lambda x: x[0])
+    return ordered_data
 
 
 main()
