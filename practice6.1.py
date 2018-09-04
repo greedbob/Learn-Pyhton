28ac3638bf69a8915f37309df436aba8996dad04
greedbob <greedbob@foxmail.com>
2018-09-04 23:45:16 +0800

    第六章练习1
    Signed-off-by: greedbob <greedbob@foxmail.com>

---
+++ practice6.1.py
@@ -0,0 +1,69 @@
+# Question:第六章 练习一 给Point类增加方法
+# Solution:
+# Answer:
+
+"""
+This module provides the Point class.
+
+>>> point = Point()
+>>> point
+Point(0, 0)
+>>> point.x = 12
+>>> str(point)
+'(12, 0)'
+>>> a = Point(3, 4)
+>>> b = Point(3, 4)
+>>> a == b
+True
+>>> a == point
+False
+>>> a != point
+True
+"""
+
+import math
+
+
+class Point:
+
+    def __init__(self, x=0, y=0):
+        """A 2D cartesian coordinate
+
+        >>> point = Point()
+        >>> point
+        Point(0, 0)
+        """
+        self.x = x
+        self.y = y
+
+    def distance_from_origin(self):
+        """Returns the distance of the point from the origin
+
+        >>> point = Point(3, 4)
+        >>> point.distance_from_origin()
+        5.0
+        """
+        return math.hypot(self.x, self.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __repr__(self):
+        return "Point({0.x!r}, {0.y!r})".format(self)
+
+    def __str__(self):
+        return "({0.x!r}, {0.y!r})".format(self)
+
+    def __add__(self, other):
+        '''sum x and y of two points separately
+
+        >>> Point(1, 2) + Point(3, 4)
+        >>> Point(4, 6)
+        '''
+        return Point(self.x + other.x, self.y + other.y)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
